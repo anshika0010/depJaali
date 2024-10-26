fcf3f9b081ae3eee45ab8f359e37dce7a30fed60
Anshika Bhadoria <anshikabhadoria88@gmail.com>
2024-10-26 12:26:07 +0530

    Sport page

--- src/Pages/ServicePage4.jsx
+++ src/Pages/ServicePage4.jsx
@@ -1,11 +1,484 @@
 import React from 'react'
 import Header from '../Componants/Header'
 import Footer from '../Componants/Footer'
+import TestimonialService from '../Componants/TestimonialService'
 
 const ServicePage4 = () => {
   return (
 <>
 {<Header/>}
+<>
+<div className="baner">
+  <img src="img/banner-img-1.jpeg" alt="" width="100%" />
+</div>
+
+  {/* grid section start */}
+  <div className="container">
+    <div className="grid">
+      <section className="side">
+        <h3 className="text-center mt-2 mb-2">
+          Key Features of Vpestify Sports Nets
+        </h3>
+        <h4>
+          {" "}
+          <i className="bi bi-bookmark-star-fill" />
+          Versatile Solutions
+        </h4>
+        <p>
+          Our netting services encompass a wide range of sports and recreational
+          activities, including cricket, volleyball, badminton, tennis, park and
+          balcony nets.
+        </p>
+        <h4>
+          {" "}
+          <i className="bi bi-bookmark-star-fill" />
+          Durable Construction
+        </h4>
+        <p>
+          Our nets are constructed from high-quality materials such as nylon or
+          polyethylene, ensuring durability and longevity even in outdoor
+          environments.
+        </p>
+        <h4>
+          {" "}
+          <i className="bi bi-bookmark-star-fill" />
+          Custom Park Sizing
+        </h4>
+        <p>
+          We understand that every playing area is unique, so we offer custom
+          sizing options for our nets. Our team will work with you to create the
+          perfect fit for your space.
+        </p>
+        <h4>
+          {" "}
+          <i className="bi bi-bookmark-star-fill" />
+          Easy Installation
+        </h4>
+        <p>
+          Our nets are designed for easy installation, with options for both
+          permanent and temporary setups. Whether you're installing nets in a
+          park for a weekend tournament or on a balcony for year-round use.
+        </p>
+        <h4>
+          {" "}
+          <i className="bi bi-bookmark-star-fill" />
+          Balcony Safety
+        </h4>
+        <p>
+          Safety is our top priority; our balcony nets provide peace of mind for
+          all ages. Whether you have a kid in your home or elderly, with our
+          netting solutions you can be tension-free.
+        </p>
+        <h4>
+          {" "}
+          <i className="bi bi-bookmark-star-fill" />
+          Nationwide Shipping
+        </h4>
+        <p>Get your sports nets delivered pan India, wherever you play.</p>
+        <h4>For inquiry or our services please call us on +91 9806161974</h4>
+      </section>
+      {/* <article class="main">
+    <h1>The Little Prince</h1>
+    <p>
+    And he went back to meet the fox.
+"Goodbye," he said.
+"Goodbye," said the fox. "And now here is my secret, a very simple secret: It is only with the heart that 
+    </p>
+  </article> */}
+    </div>
+  </div>
+  {/* grid section end */}
+  <h3 className="text-center mt-3">Installation Process</h3>
+  {/* grid section start */}
+  <div className="container">
+    <div className="grid">
+      <section className="side">
+        <h4>
+          {" "}
+          <i className="bi bi-bookmark-star-fill" /> Pre-Installation free
+          Inspection
+        </h4>
+        <p>
+          We conduct a thorough site assessment to identify potential hazards
+          and determine the appropriate netting solution and then evaluate the
+          structural integrity of the installation area to ensure it can support
+          the weight and tension of the net.
+        </p>
+        <h4>
+          {" "}
+          <i className="bi bi-bookmark-star-fill" /> Customised Design and
+          Planning
+        </h4>
+        <p>
+          Afterward, we Work closely with clients to understand their specific
+          needs and tailor the netting solution accordingly to develop a
+          detailed installation plan that includes precise measurements, anchor
+          points, and attachment methods.
+        </p>
+        <h4>
+          {" "}
+          <i className="bi bi-bookmark-star-fill" /> Professional Installation
+        </h4>
+        <p>
+          Deploy a team of experienced technicians equipped with the necessary
+          tools and equipment to safely and efficiently install the nets.
+        </p>
+      </section>
+    </div>
+  </div>
+  <>
+  {/* image container start*/}
+  <div className="container mt-4 mb-4 pt-5">
+    <div className="col-lg-12">
+      <div className="row">
+        <div className="col-lg-6 border">
+          <img src="img/nets-3.webp" alt="" width="100%" className="mt-5" />
+        </div>
+        <div className="col-lg-6 icons mt-5">
+          <p>
+            <span className="bi bi-arrow-right-circle" />
+            EXPERT ADVICE Our team guides you to help you choose the perfect
+            nets for your needs
+          </p>
+          <p>
+            <span className="bi bi-arrow-right-circle" />
+            SAFETY ASSURANCE Nets designed to meet safety standards, protecting
+            players and spectators alike
+          </p>
+          <p>
+            <span className="bi bi-arrow-right-circle" />
+            COMPETITIVE PRICING Affordable options without compromising on
+            quality
+          </p>
+        </div>
+        <h2 className="text-center mt-4">
+          For inquiry or our services please call us on{" "}
+          <span style={{ color: "#F0810F" }}>+91 9806161974</span>
+        </h2>
+      </div>
+    </div>
+  </div>
+  {/* image container end*/}
+  {/* image gallery  section start */}
+  <div className="half-half-image-text">
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          {/* <h1 class="text-center">Industrial Pigeon Netting</h1> */}
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-lg-6">
+          <div className="content">
+            <p>
+              {" "}
+              <b>Criket Practice Net</b> Enhance your cricket training
+              experience with our top-of-the-line cricket nets, designed to
+              provide a safe and controlled environment for players of all
+              levels. Our nets are crafted from high-quality, UV-stabilized
+              materials, ensuring durability and longevity even in harsh weather
+              conditions. Whether you're setting up a practice area in your
+              backyard or equipping a professional training facility, our
+              customizable and easy-to-install nets offer the perfect solution.
+              Focus on improving your technique without the worry of lost balls
+              or injuries, thanks to our robust and reliable netting solutions.
+              Join schools, clubs, and professional teams who trust our cricket
+              nets to elevate their game and practice with confidence.
+              <br />
+            </p>
+          </div>
+        </div>
+        <div className="col-12 col-lg-6">
+          <img src="img/net-image.webp" alt="" height="80%" width="100%" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="half-half-image-text">
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          {/* <h1 class="text-center">Top Bird Spike Manufacturers</h1> */}
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-lg-6">
+          <img src="img/net-image.webp" alt="" height="80%" width="100%" />
+        </div>
+        <div className="col-12 col-lg-6">
+          <div className="content">
+            <p>
+              {" "}
+              <b>Football Practice Net</b>Upgrade your football training and
+              gameplay with our premium football nets, engineered to deliver
+              superior performance and durability. Constructed from
+              high-strength, weather-resistant materials, our nets ensure
+              reliable use in any climate, making them perfect for both indoor
+              and outdoor settings. Whether you're outfitting a school field, a
+              community sports complex, or your own backyard, our football nets
+              provide the perfect balance of strength and flexibility to
+              withstand powerful shots and daily use. Easy to install and
+              available in various sizes, our nets are the preferred choice for
+              teams and players aiming to enhance their practice sessions and
+              match experiences. Trust our netting solutions to keep the game
+              going strong, season after season.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="half-half-image-text">
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          {/* <h1 class="text-center">Industrial Pigeon Netting</h1> */}
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-lg-6">
+          <div className="content">
+            <p>
+              {" "}
+              <b>Cricket Practice Net</b> Enhanced Training: Our cricket nets
+              provide a safe and controlled environment for players to practice
+              their skills without the risk of losing balls.
+              <br />
+              <b>Durable Material:</b> Constructed from high-quality,
+              UV-stabilized materials to withstand harsh weather conditions and
+              heavy usage, ensuring long-lasting performance.
+              <br />
+              <b>Customisable Sizes:</b> Available in various sizes to fit
+              different training grounds, from backyard practice setups to
+              professional training facilities.
+              <br />
+              <b>Safety First:</b> Designed to protect players, coaches, and
+              spectators by containing balls within the practice area, reducing
+              the risk of injury.
+              <br />
+              <b>Portable Options:</b> Lightweight and portable designs are
+              available, making it easy to set up and dismantle for storage or
+              transport to different locations.
+              <br />
+              <b>Affordable Solutions:</b> Competitive pricing ensures that you
+              get high-quality cricket nets without breaking the bank, making
+              them accessible for schools, clubs, and individual enthusiasts.
+              <br />
+              <b>Enhanced Focus:</b> By minimizing distractions and
+              interruptions, cricket nets help players concentrate on improving
+              their technique and honing their skills.
+              <br />
+              <b>Weather-Resistant:</b> Engineered to endure all weather
+              conditions, ensuring that practice can continue rain or shine
+              without compromising the net's integrity.
+              <br />
+              <b>Professional Quality:</b> Used by cricket academies and
+              professional teams, our cricket nets meet the highest standards of
+              quality and performance for serious training.
+              <br />
+            </p>
+          </div>
+        </div>
+        <div className="col-12 col-lg-6">
+          <img src="img/net-image.webp" alt="" height="80%" width="100%" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="half-half-image-text">
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          {/* <h1 class="text-center">Top Bird Spike Manufacturers</h1> */}
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-lg-6">
+          <img src="img/net-image.webp" alt="" height="80%" width="100%" />
+        </div>
+        <div className="col-12 col-lg-6">
+          <div className="content">
+            <p>
+              {" "}
+              <b>Football Practice Net</b> High Durability: Made from
+              high-strength, weather-resistant materials to withstand powerful
+              shots and adverse weather conditions, ensuring long-lasting use.
+              <br />
+              <b>Variety of Sizes:</b> Available in multiple sizes to
+              accommodate different field dimensions, from backyard setups to
+              professional stadiums.
+              <br />
+              <b>Easy Installation:</b> Designed for a quick and hassle-free
+              setup, allowing you to get your field ready for play in no time.
+              <br />
+              <b>Reliable Performance:</b> Engineered to provide consistent and
+              reliable performance, maintaining their shape and integrity even
+              with frequent, intense use.
+              <br />
+              <b>Safety First:</b> Helps keep players safe by preventing balls
+              from escaping the play area, reducing the risk of injury and
+              property damage.
+              <br />
+              <b>Versatile Use:</b> Suitable for various levels of play,
+              including schools, recreational leagues, and professional teams.
+              <br />
+              <b>Cost-Effective:</b> Affordable pricing without compromising on
+              quality, making it accessible for teams and facilities of all
+              budgets.
+              <br />
+              <b>Enhanced Play Experience:</b> Provides a professional look and
+              feel to any field, enhancing the overall play experience for
+              players and spectators alike.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  {/* image gallery section end */}
+  {/* accordian section start */}
+  <div className="container">
+    <h1 className="text-center">General Questions</h1>
+    <div className="accordion" id="accordionExample">
+      <div className="accordion-item">
+        <h2 className="accordion-header" id="headingOne">
+          <button
+            className="accordion-button"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseOne"
+            aria-expanded="true"
+            aria-controls="collapseOne"
+          >
+            What types of sports nets do you offer?
+          </button>
+        </h2>
+        <div
+          id="collapseOne"
+          className="accordion-collapse collapse show"
+          aria-labelledby="headingOne"
+          data-bs-parent="#accordionExample"
+        >
+          <div className="accordion-body">
+            We provide a wide range of sports nets tailored to various
+            activities and requirements. Whether you need nets for soccer,
+            tennis, golf, baseball, or any other sport, we have you covered.
+          </div>
+        </div>
+      </div>
+      <div className="accordion-item">
+        <h2 className="accordion-header" id="headingTwo">
+          <button
+            className="accordion-button collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseTwo"
+            aria-expanded="false"
+            aria-controls="collapseTwo"
+          >
+            Are your sports nets customizable?
+          </button>
+        </h2>
+        <div
+          id="collapseTwo"
+          className="accordion-collapse collapse"
+          aria-labelledby="headingTwo"
+          data-bs-parent="#accordionExample"
+        >
+          <div className="accordion-body">
+            Yes, we offer customization options to ensure that our sports nets
+            meet your specific needs. Whether it's size, shape, or material, we
+            can tailor the nets to fit your requirements.
+          </div>
+        </div>
+      </div>
+      <div className="accordion-item">
+        <h2 className="accordion-header" id="headingThree">
+          <button
+            className="accordion-button collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseThree"
+            aria-expanded="false"
+            aria-controls="collapseThree"
+          >
+            What materials are your sports nets made from?
+          </button>
+        </h2>
+        <div
+          id="collapseThree"
+          className="accordion-collapse collapse"
+          aria-labelledby="headingThree"
+          data-bs-parent="#accordionExample"
+        >
+          <div className="accordion-body">
+            Our sports nets are made from high-quality materials such as nylon,
+            polyester, or polyethylene, depending on the application. These
+            materials are durable, weather-resistant, and provide excellent
+            performance on the field.
+          </div>
+        </div>
+      </div>
+      <div className="accordion-item">
+        <h2 className="accordion-header" id="headingfour">
+          <button
+            className="accordion-button collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapsefour"
+            aria-expanded="false"
+            aria-controls="collapsefour"
+          >
+            Do you provide installation services for sports nets?
+          </button>
+        </h2>
+        <div
+          id="collapsefour"
+          className="accordion-collapse collapse"
+          aria-labelledby="headingfour"
+          data-bs-parent="#accordionExample"
+        >
+          <div className="accordion-body">
+            Yes, we offer professional installation services to ensure that your
+            sports nets are set up correctly and securely. Our experienced team
+            will handle the installation process efficiently and safely.
+          </div>
+        </div>
+      </div>
+      <div className="accordion-item">
+        <h2 className="accordion-header" id="headingfive">
+          <button
+            className="accordion-button collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapsefive"
+            aria-expanded="false"
+            aria-controls="collapsefive"
+          >
+            Can you ship sports nets pan India?
+          </button>
+        </h2>
+        <div
+          id="collapsefive"
+          className="accordion-collapse collapse"
+          aria-labelledby="headingfive"
+          data-bs-parent="#accordionExample"
+        >
+          <div className="accordion-body">
+            Yes, we can arrange shipping for our sports nets to customers
+            nationwide. Whether you're a sports facility, school, or individual
+            athlete, we'll ensure that your order reaches you promptly and in
+            excellent condition
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  {/* accordian section End */}
+</>
+
+</>
+{<TestimonialService/>}
 {<Footer/>}
 </>
   )
